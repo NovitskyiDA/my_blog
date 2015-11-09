6d07270bdb65dd25c12988385f241232913f8109
NovitskyiDA <allfayer@gmail.com>
2015-11-10 01:02:16 +0200

    added search posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true, length: 5..100
   validates :body, presence: true
   validates :user_id, presence: true
+
+  def self.search(search)
+    if search
+      where("body LIKE ?", "%#{search}%")
+    end
+  end
+  
 end
